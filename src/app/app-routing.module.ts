237897a6f3b252d6b6252f55615c290b8c7417e6
anushanthR <anushanu@live.com>
2018-10-21 18:54:11 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,10 @@
-import { AdminComponent } from './admin/admin.component';
+import { AddUserComponent } from './Components/add-user/add-user.component';
+import { AddRoleComponent } from './Components/add-role/add-role.component';
+import { AddHolidayComponent } from './Components/add-holiday/add-holiday.component';
+import { AddStatusComponent } from './Components/add-status/add-status.component';
+import { AddLeaveTypeComponent } from './Components/add-leave-type/add-leave-type.component';
+import { AddDepartmentComponent } from './Components/add-department/add-department.component';
+import { LoginComponent } from './login/login.component';
 import { ReqLeaveComponent } from './Components/req-leave/req-leave.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -8,11 +14,15 @@ import { ViewRoleComponent } from './Components/view-role/view-role.component';
 
 const routes: Routes = [
   {path: 'user', component: UserComponent},
-  {path: 'leaverequest', component: ReqLeaveComponent},
   {path: 'previlage', component: PrevilageComponent},
-  {path: 'admin', component: AdminComponent},
-  {path: 'role', component: ViewRoleComponent},
-  {path: '**', redirectTo: 'role'}
+  {path: 'login', component: LoginComponent},
+  {path: 'department', component: AddDepartmentComponent},
+  {path: 'leavetype', component: AddLeaveTypeComponent},
+  {path: 'status', component: AddStatusComponent},
+  {path: 'holiday', component: AddHolidayComponent},
+  {path: 'enrole', component: AddUserComponent},
+  {path: 'role', component: AddRoleComponent},
+  {path: '**', redirectTo: 'login'}
 ];
  @NgModule({
   imports: [RouterModule.forRoot(routes)],
