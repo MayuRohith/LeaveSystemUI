ee86af28d88506ced145e08cff6803af686ac910
thirupparan <thirupparan1994@gmail.com>
2018-10-25 22:33:58 +0530

    user add table refresh function done

--- src/app/Components/user-profile/user-profile.component.ts
+++ src/app/Components/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/Models/user';
+import { InteractionService } from 'src/app/UIService/interaction.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,10 +12,14 @@ export class UserProfileComponent implements OnInit {
 
   users: User[];
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,private interactionService:InteractionService) { }
 
   ngOnInit() {
     this.getAllUsers();
+    this.interactionService.msgDataSource$.subscribe(msg =>{
+      console.log('Update',msg);
+      this.getAllUsers();
+    })
   }
 
   getAllUsers(){
