c415d272f20e1ffcb2b799efbfc60a07cebebad3
thirupparan <thirupparan1994@gmail.com>
2018-10-31 10:26:44 +0530

    login Error message fiexed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../Services/login.service';
 import { Login } from '../Models/login.model';
 import { Router } from '@angular/router';
+import { InteractionService } from '../UIService/interaction.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,23 +12,35 @@ export class LoginComponent implements OnInit {
 
   public login: Login = new Login();
   logedUser: any;
-  constructor(private loginService: LoginService, private router: Router) { }
+  msg = 'invalid';
+  responseMsg: string;
+  constructor(private loginService: LoginService, private router: Router,private interactionService: InteractionService) { }
 
   ngOnInit() {
+    this.getResponse();
+    this.getLoginAuth();
+  }
+
+  getLoginAuth() {
+    this.loginService.getLoginAuth(this.login);
     this.loginService.loginCredential$.subscribe(data => {
       if(data != null){
         this.router.navigate(["/dashboard"]);
       }
     });
+    if (this.msg == 'invalid') {
+      this.interactionService.upadateMsg('invalid');
+    }
+  }
+  getResponse() {
+    this.interactionService.msgDataSource$.subscribe(data => {
+      this.responseMsg = data;
+      console.log(data);
+      setTimeout(() => {
+        this.responseMsg=null;
+      }, 5000);
+    });
   }
 
-  getLoginAuth() {
-    // alert("login called");
-    this.loginService.getLoginAuth(this.login);
-   
-    // this.loginService.loginCredential$.subscribe(data => {
-    //   alert(data);
-    // });
-  }
 
 }
